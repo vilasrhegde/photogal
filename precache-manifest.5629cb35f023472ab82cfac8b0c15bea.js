2395d827a0f8915a7bf161a276bf526160ee4289
vilasrhegde <vilasrhegde@gmail.com>
2022-10-16 11:49:40 +0530

    Updates

---
+++ precache-manifest.5629cb35f023472ab82cfac8b0c15bea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa84b2e7ecec5237e3f4fc8c4ae16c3",
+    "url": "/photogal/index.html"
+  },
+  {
+    "revision": "5a1c2277fd3fe968e948",
+    "url": "/photogal/static/css/main.8a9e3228.chunk.css"
+  },
+  {
+    "revision": "eb1071c82115d07cb640",
+    "url": "/photogal/static/js/2.183976ca.chunk.js"
+  },
+  {
+    "revision": "52652a6dd1376280bbfc7b1cfc4bca6a",
+    "url": "/photogal/static/js/2.183976ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a1c2277fd3fe968e948",
+    "url": "/photogal/static/js/main.7a0f34c0.chunk.js"
+  },
+  {
+    "revision": "f3b42880515fb00a3840",
+    "url": "/photogal/static/js/runtime-main.3de22a5d.js"
+  }
+]);
